5a96efcf46b5d96c7f9b5735a88133560d32dbae
Stukan Andrew <100589528+Andrew1341@users.noreply.github.com>
2023-12-08 22:26:46 +0200

    Добавьте файлы проекта.

---
+++ Client/NetClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class NetClient
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Stukan";
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Client is starting ...\nPlease wait ...");
+
+            try
+            {
+                Console.Write("Enter Server IP: ");
+                string serverIP = Console.ReadLine();
+                IPAddress ipAddr = IPAddress.Parse(serverIP);
+
+                Console.Write("Enter Server Port: ");
+                string serverPort = Console.ReadLine();
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, int.Parse(serverPort));
+
+                SendMessage(ipAddr, ipEndPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Write("Client is stopped \nPress any key to continue . . . ");
+                Console.ReadKey(true);
+            }
+        }
+
+        static void SendMessage(IPAddress ipAddr, IPEndPoint ipEndPoint)
+        {
+            using (Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                sender.Connect(ipEndPoint);
+
+                byte[] bytes = new byte[1024];
+                Console.Write("Input message: ");
+                string message = Console.ReadLine();
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                byte[] msg = Encoding.UTF8.GetBytes(message);
+                int bytesSent = sender.Send(msg);
+
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("\nServer response: {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+                if (message.IndexOf("<TheEnd>") == -1)
+                    SendMessage(ipAddr, ipEndPoint);
+
+                sender.Shutdown(SocketShutdown.Both);
+            }
+        }
+    }
+}
---
+++ Server/NetServer.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class NetServer
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Stukan";           
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Server is starting...\nPlease wait...");
+
+            try
+            {
+                Console.Write("Enter Server IP: ");
+                string serverIP = Console.ReadLine();
+                IPAddress ipAddr = IPAddress.Parse(serverIP);
+
+                Console.Write("Enter Server Port: ");
+                string serverPort = Console.ReadLine();
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, int.Parse(serverPort));
+
+                using (Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sListener.Bind(ipEndPoint);
+                    sListener.Listen(10);
+
+                    while (true)
+                    {
+                        Console.WriteLine("Waiting for connection on port {0}", ipEndPoint);
+                        using (Socket handler = sListener.Accept())
+                        {
+                            string data = null;
+                            byte[] bytes = new byte[1024];
+                            int bytesRec = handler.Receive(bytes);
+                            data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                            Console.Write("Received message: " + data + "\n\n");
+
+                            string reply = "Thank you for the reply in " + data.Length.ToString() + " symbols";
+                            byte[] msg = Encoding.UTF8.GetBytes(reply);
+                            handler.Send(msg);
+
+                            if (data.IndexOf("<TheEnd>") > -1)
+                            {
+                                Console.WriteLine("Connection closed");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
